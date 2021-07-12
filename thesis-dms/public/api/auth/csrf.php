ed0a7d50eada140c9868ec4182f9919a27b83c94
seevrus <tiller2004@gmail.com>
2021-07-12 19:58:43 +0200

    Updated CSRF token generation

--- thesis-dms/public/api/auth/csrf.php
+++ thesis-dms/public/api/auth/csrf.php
@@ -4,12 +4,14 @@ header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (empty($_SERVER['HTTP_X_CSRF_TOKEN'])) {
         session_start();
-        $token = bin2hex(random_bytes(32));
-        $_SESSION['X_CSRF_TOKEN'] = $token;
+        if (empty($_SESSION['X_CSRF_TOKEN'])) {
+            $token = bin2hex(random_bytes(32));
+            $_SESSION['X_CSRF_TOKEN'] = $token;
+        }
         
         echo json_encode(
             array(
-                'csrfToken' => $token
+                'csrfToken' => $_SESSION['X_CSRF_TOKEN']
             )
         );
     } else {
